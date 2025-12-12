d50f478e580b0ecd334834fdcd525220d8aa4a47
Jeetsikder <jeetsikder78@gmail.com>
2025-12-12 20:30:33 +0530

    Project express setup and server restart command update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
+import express, { Request, Response } from "express";
 import { prisma } from "./lib/prisma";
+const http = require("http");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+// * --------------- Server ------------
+const server = http.createServer(app);
 
 prisma
   .$connect()
@@ -8,3 +19,11 @@ prisma
   .catch((error) => {
     console.error("Error connecting to the database:", error);
   });
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from TypeScript Express Prisma!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
